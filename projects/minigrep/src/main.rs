8fe4f8b70524e68b1f09a2135b2babd2bb55296e
mei30 <meisamjafarimosleh@gmail.com>
2023-07-12 15:23:41 +0330

    Improve readability and performance by iterators

--- projects/minigrep/src/main.rs
+++ projects/minigrep/src/main.rs
@@ -4,15 +4,13 @@ use std::process;
 use minigrep::Config;
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-
-    let config = Config::new(&args).unwrap_or_else(|error| {
-        println!("Problem parsing arguments: {} ", error);
+    let config = Config::new(env::args()).unwrap_or_else(|error| {
+        eprintln!("Problem parsing arguments: {} ", error);
         process::exit(1);
     });
 
     if let Err(e) = minigrep::run(config) {
-        println!("Application Error {}", e);
+        eprintln!("Application Error {}", e);
         process::exit(1);
     }
 }
